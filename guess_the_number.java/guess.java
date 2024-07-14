b232e819b126406a7e2bc284241a652d16bafe23
prince agarwal <princeagarwalofficial@gmail.com>
2024-07-14 17:05:56 +0530

    Add files via upload

---
+++ guess_the_number.java/guess.java
@@ -0,0 +1,10 @@
+
+import java.util.Random;
+public class guess {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int random = r.nextInt(100);
+        System.out.println(random);
+    }
+    
+}
